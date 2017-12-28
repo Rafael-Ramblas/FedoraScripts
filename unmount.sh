75b29f6c48c9b0618c17d5d07f38a8b3adb00a90
Rafael Ramblas <rafaelmramblas@gmail.com>
2017-12-28 02:05:15 -0200

    Update unmount.sh

--- unmount.sh
+++ unmount.sh
@@ -1,4 +1,7 @@
+#Found in https://grimhacker.com/2016/01/22/using-bitlocker-to-go-on-fedora-23-dislocker/
+#Run as SU    
+#Run fdisk -l and find out what driver you want to unmount. Ex: sdd/sdc/sdb ...
 cd /mnt
 umount /mnt/dislocker
 umount /mnt/dislocker-container
-udisksctl power-off -b /dev/sdd
+udisksctl power-off -b /dev/<Selected Hard Drive>
